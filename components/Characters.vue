5a96718da5a2624399e2115c6db8d45ad24f1074
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2021-07-13 12:50:25 -0400

    update readme

---
+++ components/Characters.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-card-group columns class="mt-3">
+      <b-card
+        v-for="character in characters"
+        :key="character.id"
+        :title="character.name"
+        :img-src="character.imgUrl"
+        img-alt="Image"
+        img-top
+      >
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+import { characters } from '../data/index'
+
+export default {
+  data: () => ({
+    characters,
+  }),
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-navbar toggleable="sm" type="light" variant="light">
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+      <b-navbar-brand>Nuxt con Bootstrap</b-navbar-brand>
+
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-text>Home</b-nav-text>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-container>
+      <Characters />
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
